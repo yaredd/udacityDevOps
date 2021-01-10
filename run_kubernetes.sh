c749bfb4ad11cd99a3abe39b45ea908920ac6d43
Yared Deneke <yared.deneke@gmail.com>
2021-01-10 19:04:15 +0300

    final with my changes

---
+++ run_kubernetes.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+# dockerpath=<>
+dockerpath=yaredd/udacity:latest
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl create deployment k8-udacity --image=docker.io/yaredd/udacity:latest
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl expose deployment k8-udacity --name=k8-udacity --type=NodePort --port 8000 --target-port 80
